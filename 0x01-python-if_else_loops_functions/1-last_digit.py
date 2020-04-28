efe6f4d505214653548fe08152f230600d7e11eb
Diego <vandeldiegoc@gmail.com>
2020-04-28 15:05:47 -0500

    delete space and corection in condicional

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 5:
-    print ("Last digit of {} and is {} greater than 5".format(number, number % 10))
-elif number % 10 == 0:
-    print ("Last digit of {} is 0 and is 0".format(number))
+if (number % 10) > 5:
+    print("Last digit of {:d} and is {:d} greater than 5".format(number, number % 10))
+elif (number % 10) == 0:
+    print("Last digit of {:d} is 0 and is 0".format(number % 10))
 else:
-    if number < 0:  
-        print ("Last digit of {} is {} and is less than 6 and not 0".format(number, number % -10))
+    if (number % 10) < 0:
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, number % -10))
     else:
-        print ("Last digit of {} is {} and is less than 6 and not 0".format(number, number % 10))
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, number % 10))
