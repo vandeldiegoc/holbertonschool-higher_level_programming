6eb562a752d4578bb71102391d6e32577c0a0a6b
Diego <vandeldiegoc@gmail.com>
2020-04-28 21:30:02 -0500

    9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        nn = number * -1
+        print("{:d}".format(nn % 10), end="")
+    else:
+        print("{:d}".format(number % 10), end=(""))
+    return number % 10
