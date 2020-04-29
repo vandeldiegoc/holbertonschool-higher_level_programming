ecac6fe4262e00003ae69b1394bb77fccbbbdfd1
Diego <vandeldiegoc@gmail.com>
2020-04-28 19:24:04 -0500

    6 2 digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+for i in range(0, 9):
     j= 1 + i
     if i == 0:
         j = 0
-    for j in range(j, 10):
+    for j in range(j, 9):
         print("{:d}{:d}".format(i, j), end=", ")
-print(89)        
+print(89)
