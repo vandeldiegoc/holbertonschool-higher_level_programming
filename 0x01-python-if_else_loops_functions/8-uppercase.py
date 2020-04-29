3ce0956e8e0622942ab6bb53612fe8d095ef4f1e
Diego <vandeldiegoc@gmail.com>
2020-04-28 20:57:01 -0500

    check 6

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in str:
         j = ord(i)
         if  j >= 97 and j <= 122:
-            j = j - 32
+            j = j-32
         print('{:c}'.format(j), end="")
     print("")
