b395a604fab09f5b0ea4ddf511bc53e8e17dc5b4
Diego <vandeldiegoc@gmail.com>
2020-04-30 18:59:54 -0500

    2wq

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,5 +2,5 @@
 import sys
 if __name__ == '__main__':
     print("{} arguments:".format(len(sys.argv[1])))
-    for i in range(1, len(sys.argv[1:])):
+    for i in range(1, len(sys.argv)):
         print("{:d}: {}".format(i, sys.argv[i]))
