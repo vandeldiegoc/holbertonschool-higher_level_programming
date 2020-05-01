5b25b439b976f8ac417bebb593a47952c9e0fc08
Diego <vandeldiegoc@gmail.com>
2020-04-30 22:18:03 -0500

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
-    for i in dir():
-        if i[1] != "_":
-            print(i)
-#   for a in dir(hidden_4):
-#        if not a.startwith("__"):
-#            print("{}".format(a))
+    for defined in dir():
+        if defined[1] != "_":
+            print(defined)
