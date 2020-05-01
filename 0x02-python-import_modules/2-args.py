d7128dff8d60dcf85472ec911964d9db8f92e53f
Diego <vandeldiegoc@gmail.com>
2020-04-30 22:15:30 -0500

    conditions

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{:d} arguments:".format(len(sys.argv)-1))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {}".format(i, sys.argv[i]))
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(len(sys.argv)-1))
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv)-1))
+        print("{:d}: {}".format(1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv)-1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    for a in dir(hidden_4):
-        if not a.startwith("__"):
-            print("{}".format(a))
+    from hidden_4 import *
+    for i in dir():
+        if i[1] != "_":
+            print(i)
+#   for a in dir(hidden_4):
+#        if not a.startwith("__"):
+#            print("{}".format(a))
