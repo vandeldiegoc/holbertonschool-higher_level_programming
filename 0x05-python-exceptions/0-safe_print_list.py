5d146616b0348fee214731cdc2e53ec9cb1074cd
Diego <vandeldiegoc@gmail.com>
2020-05-18 14:03:13 -0500

    0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for x in range(0, x):
+        try:
+            print("{}".format(my_list[x]), end="")
+        except IndexError:
+            break
+    print('')
+    return(x)
