1b7ddb192b489a205b3ebf1f0b5142660d7112a3
Diego <vandeldiegoc@gmail.com>
2020-05-19 20:53:56 -0500

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,17 @@
- if type(size) is not int:
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """Class private instance"""
+    def __init__(self, size=0):
+        """initialization attribute"""
+        self.__size = size
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        """method that return current area"""
+        return self.__size ** 2
