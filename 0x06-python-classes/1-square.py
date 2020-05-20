a92cdb9d284ccdb2036be2edfb0f053ff5533cea
Diego <vandeldiegoc@gmail.com>
2020-05-19 19:38:58 -0500

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """Class private instance"""
+    def __init__(self, size):
+        """initialization attribute"""
+        self.__size = size
