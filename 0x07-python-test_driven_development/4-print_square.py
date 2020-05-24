dba1abb20d53a717b2e9c33ce19e8865ebf94c7d
Diego <vandeldiegoc@gmail.com>
2020-05-24 14:12:16 -0500

    4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,8 +10,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if not (isinstance(size, float)) and size < 0:
-        raise TypeError("size must be an integer")
     for x in range(size):
         for y in range(size):
             print("#", end='')
