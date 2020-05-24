f95cb8b582b75216650c52af4402ab4b53433124
Diego <vandeldiegoc@gmail.com>
2020-05-23 19:36:19 -0500

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,7 +8,7 @@ def add_integer(a, b=98):
     """module fuinction that add numbers"""
     if isinstance(a, int) or isinstance(a, float):
         if isinstance(b, int) or isinstance(b, float):
-            return (a + b)
+            return int(a) + int(b)
         else:
             raise TypeError("b must be an integer")
     else:
--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -2,7 +2,7 @@
 add_integer = __import__('0-add_integer').add_integer
 
 print(add_integer(7.5, 5))
-print(add_integer(100, -2j))
+print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
 try:
