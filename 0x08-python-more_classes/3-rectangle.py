44d1495a1998b47b4fad467c0f24259e76866063
Diego <vandeldiegoc@gmail.com>
2020-05-25 22:04:34 -0500

    3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,34 +4,38 @@
 
 class Rectangle:
     """class rectangle"""
-
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
-
-    @property
-    def height(self):
-        return self.__whidth
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__height = value
+        """constructor"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        return self.__whidth
+        """width getter method"""
+        return self.__width
+
+    @property
+    def height(self):
+        """height getter method"""
+        return self.__height
 
     @width.setter
     def width(self, value):
+        """width setter method"""
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """setter method"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         return self.__height * self.__width
