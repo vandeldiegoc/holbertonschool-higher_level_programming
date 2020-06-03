047d92f54ab6fff523b146e0f86f05e1f49c5c07
Diego <vandeldiegoc@gmail.com>
2020-06-03 18:22:09 -0500

    2, 3, 4

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,7 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+"""module"""
 
 
 def write_file(filename="", text=""):
+    """write in file"""
     with open(filename, mode='w', encoding="utf-8") as filew:
         j = filew.write(text)
         return j
--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,7 +1,8 @@
-#!usr/bin/python3
-
+#/!usr/bin/python3
+"""module"""
 
 def append_write(filename="", text=""):
+    """write in file"""
     with open(filename, 'a', encoding="utf-8") as filew:
         j = filew.write(text)
         return j
