62780815494c1ac820eddd307d4afe76144afb5d
Diego <vandeldiegoc@gmail.com>
2020-06-02 23:37:23 -0500

    7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module that has a empty class"""
+
+
+class BaseGeometry:
+    """class empty"""
+    pass
+
+    def area(self):
+        """raise Exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validator"""
+        if type(value) is not int:
+            raise TypeError(name + ' must be an integer')
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')
