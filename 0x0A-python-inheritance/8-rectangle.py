9e313661f993c974795a5e2ca483f3ce1ba5faff
Diego <vandeldiegoc@gmail.com>
2020-06-02 23:54:18 -0500

    8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""module that has a empty class"""
+
+
+class BaseGeometry:
+    """class empty"""
+    pass
+
+    def area(self):
+        """raise Exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """validator"""
+        if type(value) is not int:
+            raise TypeError(name + ' must be an integer')
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    """new class"""
+
+    def __init__(self, width, height):
+        """constructor"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
