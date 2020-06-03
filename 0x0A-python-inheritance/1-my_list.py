6b9199d2d6f65bf5efe3b85e9de49686c9677ab4
Diego <vandeldiegoc@gmail.com>
2020-06-02 20:06:05 -0500

    1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """that is a module that has a class"""
 
+
 class MyList(list):
     """new class"""
-    
+
     def print_sorted(self):
         """print list sorted"""
+
         print(sorted(self))
