c39210e5c53974065e7e491fc0951ed4c3abe7c1
Diego <vandeldiegoc@gmail.com>
2020-06-03 18:02:54 -0500

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """funtion"""
-    with open(filename, encoding="utf-8") as reedf:
-        print(reedf.read())
+    with open(filename, 'r' encoding="utf-8") as reedf:
+        print(reedf.read(), end"")
