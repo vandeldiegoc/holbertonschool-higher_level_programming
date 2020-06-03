048c54af8627f9a7aeb279f9f7db83935ff55bc4
Diego <vandeldiegoc@gmail.com>
2020-06-03 18:09:50 -0500

    2

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """module"""
 
-def number_of_lines(filename="" ):
+
+def number_of_lines(filename=""):
     """return line number"""
     line_number = 0
     with open(filename) as readf:
--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,6 +1,9 @@
 #!usr/bin/python3
+"""module"""
+
 
 def read_lines(filename="", nb_lines=0):
+    """print line number"""
     line_number = 0
     with open(filename, encoding="utf-8") as readf:
         for a_line in readf:
