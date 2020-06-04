b24d5e672b2d97807f1967b158b3b404227a0a59
Diego <vandeldiegoc@gmail.com>
2020-06-03 23:30:38 -0500

    11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -5,9 +5,10 @@
 class Student:
     """new class"""
 
+
     def __init__(self, first_name, last_name, age):
         """contructor"""
-        self.first_namename = first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
