cdf7fe1203d19d204c75fc6b402b65d8011bc946
Diego <vandeldiegoc@gmail.com>
2020-08-08 18:38:57 -0500

    files 0, 1, 2[add] argv

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """conect database to python code"""
-
 import MySQLdb
 from sys import argv
 
@@ -9,11 +8,14 @@ if __name__ == '__main__':
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
+
     conn = MSQLdb.connect(host="localhost", port=3306, user="user",
                           passwd="passwd", db="db", charset="utf8")
+
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
+
     for row in query_rows:
         print(row)
     cur.close()
