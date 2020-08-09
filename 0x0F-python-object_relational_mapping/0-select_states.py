9d61842f11c350aba9cd617e7679687e6327cd6d
Diego <vandeldiegoc@gmail.com>
2020-08-08 19:55:25 -0500

    0[fix]typing error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     passwd = argv[2]
     db = argv[3]
 
-    conn = MSQLdb.connect(host="localhost", port=3306, user=user,
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user,
                           passwd=passwd, db=db, charset="utf8")
 
     cur = conn.cursor()
