7ae874d53a630407492798da2633e380a800f54b
Diego <vandeldiegoc@gmail.com>
2020-08-09 18:37:18 -0500

    6 [fix] permissions

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class States(Base):
+class State(Base):
     """class new table thas has colum id and name"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
