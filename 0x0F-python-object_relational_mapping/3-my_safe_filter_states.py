3555b17a2f4c4ea1f5267a027caebb5797d6230f
Diego <vandeldiegoc@gmail.com>
2020-08-08 21:55:24 -0500

    3[add]

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""prevent injection sql"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    name = argv[4]
+    sql = "SELECT * FROM states WHERE name='{}'ORDER BY id ASC".format(name)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user,
+                           passwd=passwd, db=db, charset="utf8")
+    cur = conn.cursor()
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1] == name:
+            print(row)
+    cur.close()
+    conn.close()
