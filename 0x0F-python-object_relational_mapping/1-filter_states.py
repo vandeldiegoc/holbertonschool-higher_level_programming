bc9d7558d3b4e44c948672c36e17497dd78fe926
Diego <vandeldiegoc@gmail.com>
2020-08-08 19:59:12 -0500

    0[fix]typing error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    conn = MySQLdb.connect(host="localhost", port=3306, user="user",
-                           passwd="passwd", db="db", charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user,
+                           passwd=passwd, db=db, charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
