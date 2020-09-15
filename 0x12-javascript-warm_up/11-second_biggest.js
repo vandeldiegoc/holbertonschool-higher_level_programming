d86fcf7cae113181ba555be5d8315552f8d195a6
diego <vandeldiegoc@gmail.com>
2020-09-15 18:40:58 -0500

    [change] var let

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
 if (process.argv.length < 4) {
   console.log('0');
 } else {
-  var myArgs = process.argv.slice(2);
+  let myArgs = process.argv.slice(2);
   myArgs.sort(function (a, b) {
     return b - a;
   });
